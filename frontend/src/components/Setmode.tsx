217a7005d3b1236b09fb7010bcb5378aa969f415
kfr <rahh28308@gmail.com>
2026-07-14 21:08:23 +0800

    a

--- frontend/src/components/Setmode.tsx
+++ frontend/src/components/Setmode.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import {useCookies} from 'react-cookie'
 export default function Setmode(){
+    const [cookies,setCookie] = useCookies(['mode'])
+    console.log(cookies)
     return (
         <div className='font-mono'>
             <div>
-                
+
             </div>
         </div>
     )
