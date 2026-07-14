ae5f3f709579e00ed453e9ffcd74c70a44ddb4a2
kfr <rahh28308@gmail.com>
2026-07-14 19:23:20 +0800

    a

--- frontend/src/components/Resistor.tsx
+++ frontend/src/components/Resistor.tsx
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 export default function Resistor(){
     type resistorTypes = 4 | 5 | 6
     type dict = {
+        color:string,
         digit:string,
         mult:number,
         tolerance:string
@@ -12,61 +13,73 @@ export default function Resistor(){
     const [type,setType] = useState<resistorTypes>(4)
     const [colorDict,setColorDict] = useState<Record<string,dict>>({
         'Black':{
+            color:"Black",
             digit:'0',
             mult:1,
             tolerance:''
         },
         'Brown':{
+            color:"Brown",
             digit:'1',
             mult:10,
             tolerance:'$\pm 1\%$'
         },
         'Red':{
+            color:"Red",
             digit:'2',
             mult:100,
             tolerance:'$\pm 2\%$'
         },
         'Orange':{
+            color:"Orange",
             digit:'3',
             mult:1000,
             tolerance:''
         },
         'Yellow':{
+            color:"Yellow",
             digit:'4',
             mult:10000,
             tolerance:''
         },
         'Green':{
+            color:"Green",
             digit:'5',
             mult:100000,
             tolerance:'$\pm 0.5\%$'
         },
         'Blue':{
+            color:"Blue",
             digit:'6',
             mult:1000000,
             tolerance:'$\pm 0.25\%$'
         },
         'Violet':{
+            color:"Violet",
             digit:'7',
             mult:10000000,
             tolerance:'$\pm 0.1\%$'
         },
         'Grey':{
+            color:"Grey",
             digit:'8',
             mult:0,
             tolerance:'$\pm 0.05\%$'
         },
         'White':{
+            color:"White",
             digit:'9',
             mult:0,
             tolerance:''
         },
         'Gold':{
+            color:"Gold",
             digit:'0',
             mult:0.1,
             tolerance:'$\pm 5\%$'
         },
         'Silver':{
+            color:"Silver",
             digit:'0',
             mult:0.01,
             tolerance:'$\pm 10\%$'
@@ -76,8 +89,9 @@ export default function Resistor(){
         const keys = Object.keys(colorDict)
         let arr = []
         for(let i = 0; i < keys.length; i++){
-            arr.push()
+            arr.push(colorDict[keys[i]])
         }
+        return arr
     }
     return (
         <div className='font-mono'>
@@ -96,7 +110,22 @@ export default function Resistor(){
                 </div>
                 <div>
                     <h4>Resistor Color Code Chart</h4>
-                    
+                    <table className='border border-black rounded-lg p-2'>
+                        <tr className='border border-black rounded-lg p-2'>
+                            <th>Color</th>
+                            <th>Digit Value</th>
+                            <th>Multiplier</th>
+                            <th>Tolerance</th>
+                        </tr>
+                    {tabulateColorCodes().map((val,idx)=>(
+                        <tr key={idx} className='border border-black rounded-lg p-2'>
+                            <td>{val.color}</td>
+                            <td>{val.digit}</td>
+                            <td>{val.mult}</td>
+                            <td>{val.tolerance}</td>
+                        </tr>
+                    ))}
+                    </table>
                 </div>
                 {type === 4 && (
                     <div className='flex justify-center items-center flex-col'>
