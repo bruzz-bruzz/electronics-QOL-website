882688a7aca48de7d4709b91f9ad6f01b0c0c8be
kfr <rahh28308@gmail.com>
2026-07-14 20:58:18 +0800

    added cookiesProvider

---
+++ frontend/src/components/Setmode.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+export default function Setmode(){
+    return (
+        <div className='font-mono'>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import Landingpage from './components/Landingpage.tsx'
 import Resistor from './components/Resistor.tsx'
 import Ohmlaw from './components/Ohmlaw.tsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import {CookiesProvider} from 'react-cookie'
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Landingpage/>}></Route>
-    <Route path='/ohmLaw' element={<Ohmlaw/>}></Route>
-    <Route path='/resistor' element={<Resistor/>}></Route>
-  </Routes>
-  </BrowserRouter>
+  <CookiesProvider>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Landingpage/>}></Route>
+      <Route path='/ohmLaw' element={<Ohmlaw/>}></Route>
+      <Route path='/resistor' element={<Resistor/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  </CookiesProvider>
 )
